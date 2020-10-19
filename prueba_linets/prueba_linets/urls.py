a28a207c58f122420d5408201aed22b1a2c04b91
Francisca <raebeb@gmail.com>
2020-10-19 20:08:10 -0300

    urls created

--- prueba_linets/prueba_linets/urls.py
+++ prueba_linets/prueba_linets/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
 
 urlpatterns = [
+    path('generate_csv/', include('generate_csv.urls')),
     path('admin/', admin.site.urls),
 ]
