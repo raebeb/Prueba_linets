64ce6d8d12bd4d4e7d28f183de4e40fb640bd7b9
Francisca <raebeb@gmail.com>
2020-10-20 21:43:02 -0300

    model changes

--- prueba_linets/prueba_linets/settings.py
+++ prueba_linets/prueba_linets/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'generate_csv',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -77,7 +78,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    },
+
+    'legacy_db': {   # this is the legacy database
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'legacy.sqlite3',
+    },
+    
 }
 
 
